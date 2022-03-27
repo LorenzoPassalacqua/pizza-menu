71366fd6e361204a03651c61e659ae841c3489e9
LorenzoPassalacqua <lorenzopassalacqua95@gmail.com>
2022-03-27 21:46:03 +0100

    update everything (working properly)

--- javapizza.js
+++ javapizza.js
@@ -1,60 +1,92 @@
+var runningTotal = 0;
+
+
 function getReceipit(){
+    var selectsize;
     var text1 = "<h3>Your order</h3>";
-    var runningtotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
-    for (var i = 0; i < sizeArray.lenght; i++) {
+    for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked == true) {
-            var selectsize = sizeArray[i].value;
+            selectsize = sizeArray[i].value;
             text1 = text1 + selectsize + "<br>";
         }
     }
-    if (selectsize === "Small pizza"){
+    if (selectsize === "small pizza"){
         sizeTotal = 6;
     }
-    else if (selectsize === "Personal pizza"){
+    else if (selectsize === "personal pizza"){
         sizeTotal = 8;
     }
-    else if (selectsize === "Medium pizza"){
+    else if (selectsize === "medium pizza"){
         sizeTotal = 10;
     }
-    else if (selectsize === "Large pizza"){
+    else if (selectsize === "large pizza"){
         sizeTotal = 14;
     }
-    else if (selectsize === "Extra large pizza"){
+    else if (selectsize === "extra large"){
         sizeTotal = 16;
     }
-    runningtotal = sizeTotal;
+    runningTotal += sizeTotal;
     console.log (selectsize + "= $" + sizeTotal +".00" );
     console.log ("size text1: " + text1);
-    console.log ("subtotal: $" + runningtotal +".00");
-    getTopping (runningtotal, text1)
-};
+    console.log ("subtotal: $" + runningTotal +".00");
+   var t = getTopping (); console.log(t)
+   var v = getVeg ();   console.log(v)
+   document.getElementById("showText").innerHTML = text1 + t + v + "<h3> Total $" + runningTotal + ".00</h3>"
+}; 
 
-function getTopping(runningtotal, text1){
+function getTopping(){
+    var text2 = "";
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
-    for (var j = 0; j < toppingArray.length; j++){
+    for (var j = 0; j < toppingArray.length; j++){ 
         if (toppingArray[j].checked){
             selectedTopping.push(toppingArray[j].value);
             console.log("selected topping item: (" + toppingArray[j].value +")");
-            text1 = text1 + toppingArray[j].value + "<br>"
+            text2 += toppingArray[j].value + "<br>"
         }
     }
 
-    var toppingCount = selectedTopping.lenght;
+    var toppingCount = selectedTopping.length; 
+    console.log(toppingCount)
     if (toppingCount > 1){
         toppingTotal = (toppingCount -1);
     }
     else {
-        toppingTotal = 0;
+        toppingTotal = toppingCount;
     }
-    runningtotal = (runningtotal + toppingTotal);
+    runningTotal += toppingTotal;
     console.log ("total selected topping item: " + toppingCount);
     console.log (toppingCount + " topping -1 free topping= " + "$" + toppingTotal + ".00");
-    console.log ("topping text1: "+ text1);
-    console.log ("purchase total: " + "$" + runningtotal + ".00");
-    document.getElementById("showText").innerHTML = text1;
-    document.getElementById("totalPrice").innerHTML = "<h3> Total $" + runningtotal + ".00</h3>";
+    console.log ("purchase total: " + "$" + runningTotal + ".00");
+    return text2;
+}
+
+function getVeg (){
+    var text3 = "";
+    var vegTotal = 0;
+    var selectedVeg = [];
+    var vegArray = document.getElementsByClassName("veg");
+    for (var n = 0; n < vegArray.length; n++){
+        if (vegArray[n].checked){
+            selectedVeg.push(vegArray[n].value);
+            console.log ("selected veggi item: (" + vegArray[n].value +")");
+            text3 += vegArray[n].value + "<br>"
+        }
+    }
+
+    var vegCount = selectedVeg.length;
+    if (vegCount > 1){
+        vegTotal = (vegCount -1);
+    }
+    else {
+        vegTotal = vegCount;
+    }
+    runningTotal += vegTotal;
+    console.log ("total selected veggies item: " + vegCount);
+    console.log (vegCount + " veggie -1 free veggi= " + "$" + vegTotal + ".00");
+    console.log ("purchase total: " + "$" + runningTotal + ".00");
+    return text3;
 }
